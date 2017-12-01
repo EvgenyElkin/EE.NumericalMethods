f1b67cdda829f20f6d6cc73294556ed5b17107ba
ee <ee@xrm.ru>
2017-12-01 12:24:00 +0500

    Добавил тесты на прогонку

---
+++ EE.NumericalMethods/EE.NumericalMethods.Tests/Helpers/AlgoritmHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using EE.NumericalMethods.Core.Helpers;
+using Xunit;
+
+namespace EE.NumericalMethods.Tests.Helpers
+{
+    public class AlgoritmHelperTests
+    {
+        [Fact]
+        public void TridiagonalMatrixAlgoritm_Success()
+        {
+            //4x1 + 3x2 = 4;
+            //x1 + 2x2 - x3 = 2;
+            //x2 + 4x3 + x4 = 7,5;
+            //x3 + 2x4 - x5 = 1;
+            //x4 + 2x5 = -3;
+            //Arrange
+            var a = new double[,]
+            {
+                { 2, -3,  0,  0,  0},
+                {-1,  8, -5,  0,  0},
+                { 0, -1, 12, -6,  0},
+                { 0,  0,  2, 18, -5},
+                { 0,  0,  0, -4, 10}
+            };
+
+            var b = new double[] {-25, 72, -69, -156, 20};
+
+            //Act
+            var result = AlgoritmHelper.TridiagonalMatrixAlgoritm(a, b);
+
+            var expected = new[] {-10, 4.999999, -2.00081, -10.001042, -3};
+            //Assert
+            Assert.Equal(expected, result, new EpsilonComparer(3));
+        }
+    }
+}
---
+++ EE.NumericalMethods/EE.NumericalMethods.Tests/Helpers/EpsilonComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EE.NumericalMethods.Tests.Helpers
+{
+    /// <summary>
+    /// Определяет равенство с определенной точностью
+    /// </summary>
+    public class EpsilonComparer : IEqualityComparer<double>
+    {
+        private readonly double _epsilon;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="power">точность, в кол-вах знаков</param>
+        public EpsilonComparer(int power)
+        {
+            _epsilon = Math.Pow(10, -power);
+        }
+
+        public bool Equals(double x, double y)
+        {
+            return Math.Abs(x - y) < 5 * _epsilon;
+        }
+
+        public int GetHashCode(double obj)
+        {
+            return 1;
+        }
+    }
+}
